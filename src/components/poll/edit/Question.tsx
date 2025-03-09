c62145b0a22e590484fdc889c1e024883e7416f1
VerySirias <Verysirias@gmail.com>
2025-03-08 23:40:49 -0500

    feat: started on question model.

--- src/components/poll/edit/Question.tsx
+++ src/components/poll/edit/Question.tsx
@@ -4,26 +4,77 @@ import {
   AccordionDetails,
   AccordionSummary,
   Typography,
+  TextField,
+  Stack
 } from "@mui/material"
+import { DragHandle } from "@mui/icons-material"
+import MenuItem from '@mui/material/MenuItem';
 
 interface Props {
-  pollID: string
-  questionID: string
+  //pollID: string
+  //questionID: string
   questionNumber: number
+  prompt: string
+  prompt_img?: string
+  options: Record<string,boolean>
+  weight: number
+  anonymous: boolean
+  time: number
+  prompt_type: "multiple-choice" | "multi-select"
 }
 
-export default function Question(props: Props) {
+export default function Question(
+  props: Props
+) {
   return (
     <Accordion>
       <AccordionSummary>
-        <Box>
-          <Typography sx={{ wordBreak: "break-word" }}>
-            Question {props.questionNumber}
+        <Box display={"flex"}>
+          <DragHandle/>
+          <Typography sx={{ wordBreak: "break-word" }} marginLeft={1}>
+            Q.{props.questionNumber}: {props.prompt}
           </Typography>
         </Box>
       </AccordionSummary>
       <AccordionDetails>
-        <Typography>hhhhhhhhhhhhhhh</Typography>
+      <Stack spacing={3}>
+        <TextField
+          label= 'Prompt'
+          variant='outlined'
+          //fullWidth
+        />
+        <TextField
+          label= 'Type of Question'
+          variant='outlined'
+          select
+          defaultValue="multiple-choice"
+          //onChange={""}
+          >
+            <MenuItem value= {"multiple-choice"}>
+              Multiple Choice
+            </MenuItem>
+            <MenuItem value= {"multi-select"}>
+              Multiple Select
+            </MenuItem>                        
+        </TextField>
+
+        {
+          Object.entries(props.options).map(([key, value]) => (
+            <TextField 
+              placeholder= {key}
+              variant='outlined'
+              key={key}
+            >
+              {key}
+            </TextField>
+          ))
+          
+        }
+          
+      
+
+
+      </Stack>
       </AccordionDetails>
     </Accordion>
   )
--- src/pages/PollEditor.tsx
+++ src/pages/PollEditor.tsx
@@ -8,9 +8,9 @@ export default function PollEditor() {
         Poll Maker!
       </Typography>
       <Stack spacing={3}>
-        <Question pollID='' questionID='' questionNumber={1} />
-        <Question pollID='' questionID='' questionNumber={2} />
-        <Question pollID='' questionID='' questionNumber={3} />
+        <Question questionNumber={1} prompt="Will Daniel spell the language of English?" prompt_img="" options={{"Yes":false, "No":true}} weight={1} anonymous={false} time={NaN} prompt_type="multiple-choice"/>
+        <Question questionNumber={2} prompt="Will Michael die from laughing?" prompt_img="" options={{"Yes":false}} weight={1} anonymous={false} time={NaN} prompt_type="multiple-choice"/>
+        <Question questionNumber={3} prompt="Fish?" prompt_img="" options={{"Yes":false}} weight={1} anonymous={false} time={NaN} prompt_type="multiple-choice"/>
       </Stack>
     </Container>
   )
