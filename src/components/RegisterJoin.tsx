a7c4e5ec22598048461dcf5141934d67421e4786
tdhillon113 <tdhillon1@pride.hofstra.edu>
2025-03-11 10:48:46 -0400

    adding to previous changes + ui changes

--- src/components/RegisterJoin.tsx
+++ src/components/RegisterJoin.tsx
@@ -1,72 +1,199 @@
-import {RA} from "@/styles"
-import { Box, Button, Container, Stack, TextField, Typography } from "@mui/material"
+// import { RA } from "@/styles"
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material"
+// import {Stack} from "@mui/material/Stack"
 import { useNavigate } from "react-router-dom"
 import { useState } from "react"
-import { createUser} from "@/core/api"
-import { auth } from "@/core/api/firebase"
+// import { createUser } from "@/core/api"
+import { auth, db } from "@/core/api/firebase"
 import useSnackbar from "@/core/hooks/useSnackbar"
-import React from "react"
+// import React from "react"
+import { doc, getDoc, setDoc } from "firebase/firestore"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import SignInWGoogleButton from "@/components/auth/SignInWGoogleButton"
+import { Visibility, VisibilityOff } from "@mui/icons-material"
 
-// export default function RegisterJoin(){
-//     const navigate = useNavigate()
-//     const [name, setProfileName] = useState<string>("")
-//     const snackbar = useSnackbar()
-//     const handleRegClick = () =>
-//     }
-// //export default function RegisterJoin()
-    // const navigate = useNavigate()
-    // const [displayName]
-    
-export default function RegisterJoin(props: { ref?: React.Ref<unknown>}) {
-    const navigate = useNavigate()
+export default function RegisterJoin() {
+  const navigate = useNavigate()
+  const snackbar = useSnackbar()
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState("")
+  const [retypePassword, setRetypePassword] = useState("")
+  const [showPassword, setShowPassword] = useState(false)
+  // const [showRetypePassword, setShowRetypePassword] = useState(false)
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+    retypePassword: "",
+  })
 
-    const [email, setEmail] = useState<string>("")
-    const [password, setPassword] = useState("")
-    const [retypePassword, setRetypePassword] = useState("")
-    const [errors, setErrors] = useState({
+  //   const snackbar = useSnackbar()
+
+  const setFieldError = (field: string, message: string) => {
+    setErrors((prev) => ({
+      ...prev,
+      [field]: message,
+    }))
+    throw new Error(message)
+  }
+
+  const validate = () => {
+    setErrors({
       email: "",
       password: "",
-      confirmPassword: ""
-  
-    });
-    const snackbar = useSnackbar(); 
+      retypePassword: "",
+    })
 
-return(
-<Box textAlign={'center'} ref={props.ref}>
-<Container
-    maxWidth ='xs'
->
-    <Typography
-        variant='h5'
-        textAlign ='center'
-        marginTop={5}
-        marginBottom={5}>
-        Register
-    </Typography>
+    try {
+      if (!email) {
+        setFieldError("email", "Email required!")
+      }
+      if (!/\S+@\S+\.\S+/.test(email)) {
+        setFieldError("email", "Invalid email")
+      }
+      if (!password) {
+        setFieldError("password", "Password required")
+      }
 
-    <Stack
-        component={'form'}
-        sx={{m: 1}}
-        spacing={3}
-        noValidate
-        autoComplete='off'>
+      if (password.length < 6) {
+        setFieldError("password", "Password to short")
+      }
 
-    <TextField
-        id='register-name'
-        label='username'
-        variant='outlined'
-        fullWidth
-        // onChange={(e)}
-/>
-<TextField
-    id='register-password'
-    label='Password'
-    fullWidth
-    // onChange={(e)} => setRegisterPassword(e.target.value)}
-/>
-</Stack>
-</Container>
-</Box>
-)
+      if (password !== retypePassword) {
+        setFieldError("retypePassword", "Passwords dont match")
+      }
+      return true
+    } catch (errors) {
+      return false
+    }
+  }
+
+  const handleRegClick = async () => {
+    try {
+      if (!validate()) {
+        return
+      }
+
+      //checking if users exists in firestore
+      const userRef = doc(db, "users", email)
+      const userSnap = await getDoc(userRef)
+
+      //   await createUser(email, password)
+
+      if (userSnap.exists()) {
+        setErrors((prev) => ({ ...prev, email: "Email registered already" }))
+        snackbar.show({ message: "Email already exists" })
+        return
+      }
+
+      const UserCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+
+      const user = UserCredential.user
+
+      //saving user to firebase
+
+      await setDoc(
+        doc(db, "users", user.uid),
+        {
+          email: user.email,
+          createdAt: new Date(),
+        },
+        { merge: true }
+      ) //data merge
+
+      snackbar.show({
+        message: "Get Ready to Poll Up",
+      })
+
+      navigate("/dashboard")
+    } catch (error: any) {
+      //error handling method used from firebase authentication page
+
+      if (error === "auth/email-already-in-use") {
+        setErrors((prev) => ({ ...prev, email: "Email is taken!" }))
+        snackbar.show({
+          message: "Email already in use, try logging in",
+        })
+      } else {
+        snackbar.show({
+          message: "Registration failed. Please try again.",
+        })
+      }
+    }
+  }
+  return (
+    <Box textAlign={"center"}>
+      <Container maxWidth='xs'>
+        <Typography
+          variant='h6'
+          textAlign='center'
+          marginTop={5}
+          marginBottom={5}>
+          Register
+        </Typography>
+        <Stack
+          component={"form"}
+          sx={{ m: 1 }}
+          spacing={3}
+          noValidate
+          autoComplete='off'>
+          <TextField
+            id='register-email'
+            label='Email'
+            variant='outlined'
+            fullWidth
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            error={!!errors.email}
+            helperText={errors.email}
+          />
+          <TextField
+            id='register-password'
+            label='Password'
+            fullWidth
+            value={password}
+            type={showPassword ? "text" : "password"}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <TextField
+            id='register-retype-password'
+            label='Retype Password'
+            fullWidth
+            type={showPassword ? "text" : "password"}
+            value={retypePassword}
+            onChange={(e) => setRetypePassword(e.target.value)}
+            error={!!errors.retypePassword}
+            helperText={errors.retypePassword}
+          />
+          <Button
+            startIcon={showPassword ? <VisibilityOff /> : <Visibility />}
+            onClick={() => setShowPassword((prev) => !prev)}
+            size='small'>
+            {showPassword ? "Hide Password" : "Show Password"}
+          </Button>
+          <Button
+            variant='contained'
+            color='primary'
+            fullWidth
+            onClick={handleRegClick}>
+            Register
+          </Button>
+
+          <SignInWGoogleButton />
+
+          {/* </FormatItalicIcon> */}
+        </Stack>
+      </Container>
+    </Box>
+  )
 }
-
