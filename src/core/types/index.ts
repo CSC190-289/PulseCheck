f30420a6d5e95fea49aa5f68d3304955735257fd
CamposmDev <camposm.dev@gmail.com>
2025-03-14 18:52:00 -0400

    chore(PollEditor): types for Poll, Question

--- src/core/types/index.ts
+++ src/core/types/index.ts
@@ -4,11 +4,32 @@ export interface User {
 }
 
 export interface Poll {
-  questions: [Question]
+  owner: string
+  title: string
+  async: boolean
+  anonymous: boolean | null
+  time: number | null
+  created_at: string
+  updated_at: string
 }
 
 export interface Question {
-  choices: [string]
+  prompt_type: PromptType
+  prompt: string
+  prompt_img: string
+  options: [PromptOption]
+  points: number
+  anonymous: boolean
+  time: number | undefined
+  created_at: Date
+  updated_at: Date
+}
+
+export type PromptType = "multiple-choice" | "multi-select" | "ranking-poll"
+
+export interface PromptOption {
+  text: string
+  correct: boolean
 }
 
 export interface Lobby {
