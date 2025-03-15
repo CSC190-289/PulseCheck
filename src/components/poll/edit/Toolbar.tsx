fe2e88638afd2f57cc563676730d2b8fc40c07a5
VerySirias <Verysirias@gmail.com>
2025-03-15 17:43:55 -0400

    feat(PollEditer): Toolbar completed

--- src/components/poll/edit/Toolbar.tsx
+++ src/components/poll/edit/Toolbar.tsx
@@ -1,5 +1,16 @@
-import { Toolbar as MUIToolbar, Typography } from "@mui/material"
-import { Timestamp } from "firebase/firestore"
+import {
+  Toolbar as MUIToolbar,
+  TextField,
+  IconButton,
+  Box,
+  Typography,
+} from "@mui/material"
+import { serverTimestamp, Timestamp } from "firebase/firestore"
+import { useEffect, useState } from "react"
+import { db } from "@/core/api/firebase"
+import { doc, updateDoc } from "firebase/firestore"
+import useSnackbar from "@/core/hooks/useSnackbar"
+import { Done, Edit } from "@mui/icons-material"
 
 interface Props {
   /* TODO - declare props here */
@@ -10,33 +21,90 @@ interface Props {
 
 /**
  * A toolbar component that allows users to edit the title of a poll.
- * Users can click on the edit icon next to the title to edit it inline and save changes.
- * Below the title should display the last time the poll was updated.
+ * Users can click on the edit icon next to the title to edit it inline and save changes with the Done icon.
+ * Below the title it will display the last time the poll was updated.
  * @author VerySirias
  * @returns {JSX.Element}
  */
 export default function Toolbar(props: Props) {
   console.debug("pe.toolbar.props", props)
-  /**
-   * @VerySirias
-   * You're going to have to learn what a useState hook is. You're going to need it in order to
-   * keep track of what the user types in the textfield. In my mind, the UI should have the
-   * following:
-   * - The poll title centered in the toolbar to show the user the name of their poll
-   * - An edit icon button next to the title (left or right?) to notify the user they can edit their
-   *    poll title.
-   *  - On click of the edit icon button, the system will replace the title with a textfield that
-   *    displays the title to allow the user to edit.
-   *  - Here's the tricky part, if the user presses "Enter" then that will save the title to firestore
-   *    for that poll, however on mobile, I won't have access to an "Enter" key. So what should I do then?
-   *    I'll leave that up to you, think about what would be most convenient for the user. Maybe a button?
-   *    Or something else?
-   * - Below the poll title is the last time the poll was updated. Up to you how it should be formatted.
-   *      - Could be `Last Updated MM:DD:YYYY` or `Last Updated 1 minute ago`, up to you!
-   */
+
+  const { pollId, lastUpdated } = props
+  const [title, setTitle] = useState(props.title)
+  const [editTitle, setEditTitle] = useState(true)
+  const snackbar = useSnackbar()
+  const handleClickEdit = () => {
+    if (editTitle === true) {
+      setEditTitle(false)
+    } else {
+      setEditTitle(true)
+    }
+    return editTitle
+  }
+
+  useEffect(() => {
+    async function savePrompt(text: string) {
+      const ref = doc(db, "polls", pollId)
+      try {
+        await updateDoc(ref, {
+          title: text,
+          lastUpdated: serverTimestamp(),
+        })
+      } catch {
+        snackbar.show({
+          message: "Failed to update",
+          type: "error",
+        })
+      }
+    }
+    if (editTitle === true) {
+      void savePrompt(title)
+    }
+  }, [pollId, title, snackbar, editTitle])
+
   return (
-    <MUIToolbar sx={{ boxShadow: "2px 2px rgba(0,0,0,0.1)" }}>
-      <Typography>VerySirias is just getting stated</Typography>
+    <MUIToolbar
+      sx={{
+        boxShadow: "2px 2px rgba(0,0,0,0.1)",
+        justifyContent: "center",
+      }}>
+      <Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexGrow: 1,
+            marginTop: 1,
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+          <TextField
+            label='Title Here'
+            hiddenLabel
+            defaultValue={props.title}
+            onChange={(e) => setTitle(e.target.value)}
+            disabled={editTitle}
+            fullWidth
+          />
+          {editTitle ? (
+            <IconButton color='primary' onClick={handleClickEdit}>
+              <Edit />
+            </IconButton>
+          ) : (
+            <IconButton color='primary' onClick={handleClickEdit}>
+              <Done />
+            </IconButton>
+          )}
+        </Box>
+        <Box
+          sx={{
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+          <Typography variant='caption'>
+            Last Updated: {lastUpdated.toDate().toDateString()}
+          </Typography>
+        </Box>
+      </Box>
     </MUIToolbar>
   )
 }
