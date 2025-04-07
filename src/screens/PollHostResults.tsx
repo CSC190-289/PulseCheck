579164925ad5b01ecb2d78d656e59bd4371fd7e3
VerySirias <Verysirias@gmail.com>
2025-04-07 16:50:44 -0400

    feat: added new components for PollHostResults + more set up

--- src/screens/PollHostResults.tsx
+++ src/screens/PollHostResults.tsx
@@ -1,11 +1,31 @@
+import { Container, Typography, Box, Stack, Divider } from "@mui/material"
+import subChart from "@/components/poll/submission/subchart"
+import scoreDetails from "@/components/poll/submission/scoreDetails"
+
 /**
- * Allows users to set the settings for a question of a poll.
+ * Allows Host to see the poll results.
  * @author ZairaGarcia17, VerySirias
  * @returns {JSX.Element}
  */
 
-import { Typography } from "@mui/material"
+/**
+ * Make components for the following:
+ *-Toolbar
+ *-Chart scores (we can reuse the one I made for participants)
+ *-Score Details (we can reuse the one I made for participants)
+ * -search up and participants card
+ * These components are in compoents/poll/submission
+ */
 
 export default function PollHostResults() {
-  return <Typography>Hello ZariraGarcia17</Typography>
+  return (
+    <Container maxWidth='xs' sx={{ textAlign: "initial" }}>
+      <Typography>Hello ZariraGarcia17</Typography>
+      <Box mt={2}>
+        <Stack> Chart here </Stack>
+        <Stack> {scoreDetails()} </Stack>
+        <Stack> participants card here </Stack>
+      </Box>
+    </Container>
+  )
 }
