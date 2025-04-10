ec67afff4a059a323b32da1978170942ea44b69b
Camputron <camposm.dev@gmail.com>
2025-04-10 16:52:10 -0400

    update(api.submissions): query filters

--- src/core/api/firebase/submissions.ts
+++ src/core/api/firebase/submissions.ts
@@ -1,4 +1,13 @@
-import { doc, DocumentReference, updateDoc } from "firebase/firestore"
+import {
+  collection,
+  CollectionReference,
+  doc,
+  DocumentReference,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore"
 import BaseStore from "./store"
 import { Submission } from "@/core/types"
 import { clx } from "."
@@ -11,10 +20,31 @@ export default class SubmissionStore extends BaseStore {
     return doc(this.db, clx.submissions, sid) as DocumentReference<Submission>
   }
 
+  public collect(): CollectionReference<Submission> {
+    return collection(
+      this.db,
+      clx.submissions
+    ) as CollectionReference<Submission>
+  }
+
   public async updateByRef(
     ref: DocumentReference<Submission>,
     payload: Partial<Submission>
   ): Promise<void> {
     await updateDoc(ref, payload)
+  }
+
+  public findAllByUID(uid: string) {
+    const userRef = doc(this.db, clx.users, uid)
+    const subsRef = collection(this.db, clx.submissions)
+    const q = query(subsRef, where("user", "==", userRef))
+    return getDocs(q)
+  }
+
+  public findAllBySID(sid: string) {
+    const sessionRef = doc(this.db, clx.sessions, sid)
+    const subsRef = this.collect()
+    const q = query(subsRef, where("session", "==", sessionRef))
+    return getDocs(q)
   }
 }
--- src/screens/Debug.tsx
+++ src/screens/Debug.tsx
@@ -1,11 +1,36 @@
 import UserAvatar from "@/components/poll/submission/UserAvatar"
-import { Container, Typography } from "@mui/material"
+import api from "@/core/api/firebase"
+import { Button, Container, Typography } from "@mui/material"
 
 export default function Debug() {
   return (
     <Container>
       <Typography>Debug Here</Typography>
       <UserAvatar uid='hi' displayName='Michael' />
+      <Button
+        onClick={() => {
+          api.submissions
+            .findAllByUID("OX8UHNB3UFX1YPKDMT4RlKdSEB43")
+            .then((x) => {
+              const docs = x.docs
+              docs.forEach((y) => console.debug(y.data()))
+            })
+            .catch((err) => console.error(err))
+        }}>
+        Find all By UID
+      </Button>
+      <Button
+        onClick={() => {
+          api.submissions
+            .findAllBySID("U3ftUVwPcy7Fthulnoym")
+            .then((x) => {
+              const docs = x.docs
+              docs.forEach((y) => console.debug(y.data()))
+            })
+            .catch((err) => console.error(err))
+        }}>
+        Find All By SID
+      </Button>
     </Container>
   )
 }
