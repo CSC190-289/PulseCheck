91cfebbdac803aa1422c8f476a31bc7d6fb07573
Bran7astic <budhanbrandon04@gmail.com>
2025-04-19 15:41:29 -0400

    Correctly rendered frequency for each option

--- src/components/poll/session/ResultsChart.tsx
+++ src/components/poll/session/ResultsChart.tsx
@@ -1,6 +1,7 @@
 import { BarChart } from "@mui/x-charts/BarChart"
 
 import { SessionQuestionResults } from "@/core/types"
+import { data } from "react-router-dom"
 
 interface Props {
   results: SessionQuestionResults
@@ -8,21 +9,29 @@ interface Props {
 
 export default function ResultsChart(props: Props) {
   const { results } = props
-  const series = Object.values(results.series).map((x) => ({ data: x.data }))
+  console.debug("results", results)
+  const series = Object.values(results.series).map((x) => ({
+    data: x.data,
+    label: x.text,
+  }))
   console.debug("series", series)
-  const xAxisData = Object.values(results.series).map((x) => x.text)
-  console.debug("xAxisData", xAxisData)
+  const yAxisData = Object.values(results.series).map((x) => x.text)
+  console.debug("yAxisData", yAxisData)
+  const dataSeries = []
+  series.forEach((x) => dataSeries.push(x.data[0]))
+  console.debug(dataSeries)
+
   /* TODO - figure out how to render the data */
   return (
     <BarChart
       //   series={series}
-      series={[
-        { data: [35, 44, 24] },
-        { data: [51, 6, 49] },
-        { data: [15, 25, 30] },
-      ]}
-      //   layout='horizontal'
-      xAxis={[{ data: xAxisData, scaleType: "band" }]}
+      // series={[
+      //   { data: [35, 44, 24] },
+      // ]}
+      //yAxis = {[{data: ["waga", "baga", "bobo"], scaleType: "band"}]}
+      series={[{data: dataSeries}]}
+      yAxis={[{ data: yAxisData, scaleType: "band" }]}
+      layout='horizontal'
       height={300}
     />
   )
