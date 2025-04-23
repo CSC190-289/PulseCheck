827f41f3c8950a7f964825b92c2a27e474ccc2ae
Bran7astic <budhanbrandon04@gmail.com>
2025-04-23 19:43:35 -0400

    Check whether user is authorized, if not, navigate to splash screen

--- src/lib/hooks/usePompeii.ts
+++ src/lib/hooks/usePompeii.ts
@@ -1,5 +1,11 @@
 import { useNavigate } from "react-router-dom"
+import { useAuthContext } from "."
 
 export default function usePompeii() {
   const navigate = useNavigate()
+  const user = useAuthContext()
+
+  if (!user) {
+    void navigate("/")
+  }
 }
